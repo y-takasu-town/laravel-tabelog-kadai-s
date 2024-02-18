683ff70351888e877a28fe8d09efc79bb5bcae33
saaya-nakamura <saaya.tsujino@gmail.com>
2024-02-18 22:17:05 +0900

    welcome.blade.phpデザイン修正

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,8 @@
 <link href="{{asset('css/nagoyameshi.css')}}" rel="stylesheet">
 
 @section('content')
-    <div class="container-fluid top-container">
-        <div class="row justify-content-center align-items-center" style="height: 70vh">
+    <div class="container-fluid">
+        <div class="row justify-content-center align-items-center" style="height: 60vh">
             <div class="col-md-6 text-center mx-auto">
                 <h1 class="display-3">NAGOYAMESHI</h1>
                 <p class="lead">名古屋のB級グルメ</p>
@@ -13,14 +13,14 @@
                 <!-- カテゴリ・店舗名フォーム -->
                 <form action="{{route('stores.index')}}" method="GET">
                 @csrf
-                    <select name="category_id">
+                    <select name="category_id" class="w-100">
                         <option disabled selected value>カテゴリを選択</option>
                         @foreach ($categories as $category)
                         <option value="{{ $category->id }}">{{ $category->name }}</option>
                         @endforeach
                     </select>
-                    <input type="name" name="name">
-                    <button type="submit">検索</button>
+                    <input type="name" name="name" class="mt-3 w-100">
+                    <button type="submit" class="mt-3 btn nagoyameshi-submit-button w-100">検索</button>
                 </form>
 
                 
