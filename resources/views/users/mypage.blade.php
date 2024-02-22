3e8712b1f37a3ce5eb41b8df62db3b943d7ed9e8
saaya-nakamura <saaya.tsujino@gmail.com>
2024-02-22 23:58:04 +0900

    マイページ　デザイン

--- resources/views/users/mypage.blade.php
+++ resources/views/users/mypage.blade.php
@@ -8,68 +8,94 @@
 </div>
 @endif
 
-<div class="row justify-content-center align-center">
+<div class="row justify-content-center align-items-center">
   <h1 class="text-center">{{$user->name}}さんのマイページ</h1>
 
-  <div class="col-5">
-    <table class="table table-striped mx-auto">
+  <div class="col">
+    <table class="table table-striped mx-auto p-2" style="width: 200px;">
       <tr>
         <a href="{{route('mypage.edit')}}">
           <th scope="row">
-            <a href="{{route('mypage.edit')}}" class="h2 text-decoration-none fw-bold">会員情報編集</a>
+            <a href="{{route('mypage.edit')}}" class="h4 text-decoration-none text-nowrap fw-bold link-body-emphasis">会員情報編集</a>
           </th>
-<div class="mx-auto">
-          <td>お名前・メールアドレスの変更</td>
-          <div>
+            <td>
+              <div class="text-nowrap">
+                お名前・メールアドレスの変更
+              </div>
+              </td>
       </tr>
 
       <tr>
-        <th scope="row">
-          <a href="{{route('mypage.edit_password')}}">パスワード変更</a>
+        <th scope="row ">
+          <a href="{{route('mypage.edit_password')}}" class="h4 text-decoration-none text-nowrap fw-bold link-body-emphasis">パスワード変更</a>
         </th>
-        <td>パスワードの変更</td>
+        <td>
+          <div class="text-nowrap">
+            パスワードの変更
+          </div>
+        </td>
       </tr>
 
       @if($user->subscribed('main'))
         <tr>
           <th scope="row">
-            <a href="{{route('mypage.favorite')}}">お気に入り一覧</a>
+            <a href="{{route('mypage.favorite')}}" class="h4 text-decoration-none text-nowrap fw-bold link-body-emphasis">お気に入り一覧</a>
           </th>
-          <td>お気に入りした店舗の一覧</td>
+          <td>
+           <div class="text-nowrap">
+             お気に入りした店舗の一覧
+           </div>
+          </td>
         </tr>
 
         <tr>
           <th scope="row">
-            <a href="{{route('mypage.reservations')}}">予約一覧</a>
+            <a href="{{route('mypage.reservations')}}" class="h4 text-decoration-none text-nowrap fw-bold link-body-emphasis">予約一覧</a>
           </th>
-          <td>ご予約の一覧</td>
+          <td>
+            <div class="text-nowrap">
+              ご予約の一覧
+            </div> 
+          </td>
         </tr>
       @endif
 
       @if(!$user->subscribed('main'))
         <tr>
           <th scope="row">
-            <a href="{{ route('subscript.index') }}">有料会員登録</a>
+            <a href="{{ route('subscript.index') }}" class="h4 text-decoration-none text-nowrap fw-bold link-body-emphasis">有料会員登録</a>
           </th>
-          <td>お気に入り追加・来店予約ができます</td>
+          <td>
+           <div class="mx-auto text-nowrap">
+             お気に入り追加・来店予約・レビューの投稿ができます
+            </div>
+          </td>
         </tr>
       @else
         <tr>
           <th scope="row" >
-            <a href="{{ route('subscript.cancel') }}">有料会員を解約する</a>
+            <a href="{{ route('subscript.cancel') }}" class="h4 text-decoration-none text-nowrap fw-bold link-body-emphasis">有料会員を解約する</a>
           </th>
-          <td>有料会員（お気に入り追加・来店予約）の利用をやめる
+          <td>
+            <div class="text-nowrap">
+              有料会員（お気に入り追加・来店予約・レビュー投稿）の利用をやめる
+            </div>
+          </td>
         </tr>
       @endif
 
       <tr>
         <th scope="row">
-          <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">ログアウト</a>
+          <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="h4 text-decoration-none text-nowrap fw-bold link-body-emphasis">ログアウト</a>
           <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
           @csrf
           </form>
         </th>
-        <td>ログアウトする</td>
+        <td>
+          <div class="text-nowrap">
+            ログアウトする
+          </div>
+        </td>
       </tr>
     </table>
   </div>
